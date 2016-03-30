f1cd0ac3fe2e749f7040552d5eaa6efd3b19fd8a
Bert Spaan <bert.spaan@gmail.com>
2016-03-30 16:32:36 -0400

    new data module structure

--- centerlines.js
+++ centerlines.js
@@ -1,32 +1,26 @@
-'use strict';
-var fs = require('fs');
-var path = require('path');
-var crypto = require('crypto');
-var H = require('highland');
-var R = require('ramda');
-var JSONStream = require('JSONStream');
+'use strict'
+var fs = require('fs')
+var path = require('path')
+var crypto = require('crypto')
+var H = require('highland')
+var R = require('ramda')
+var JSONStream = require('JSONStream')
 
 var files = [
   'manhattan',
   'brooklyn'
-];
+]
 
-var writeObjects = function(writer, object, callback) {
-  writer.writeObject(object, function(err) {
-    callback(err);
-  });
-};
-
-function streetName(feature) {
+function streetName (feature) {
   return feature.properties.streetname
 }
 
-function convertFeatures(features) {
-  var ids = features.map(function(f) {
-    return f.properties.id;
-  }).join(',');
-  var id = crypto.createHash('md5').update(ids).digest('hex');
-  var name = streetName(features[0]);
+function convertFeatures (features) {
+  var ids = features.map(function (f) {
+    return f.properties.id
+  }).join(',')
+  var id = crypto.createHash('md5').update(ids).digest('hex')
+  var name = streetName(features[0])
 
   return {
     type: 'pit',
@@ -38,47 +32,44 @@ function convertFeatures(features) {
       validUntil: 1862,
       geometry: {
         type: 'MultiLineString',
-        coordinates: features.map(function(f) {
-          return f.geometry.coordinates;
+        coordinates: features.map(function (f) {
+          return f.geometry.coordinates
         })
       }
     }
-  };
+  }
 }
 
-function convert(config, dir, writer, callback) {
+function transform (config, dirs, tools, callback) {
   var streams = files
-    .map(file => `./data/${file}.geojson`)
-    .map(filename => fs.createReadStream(path.join(__dirname, filename))
-      .pipe(JSONStream.parse('features.*')));
+    .map((file) => `./data/${file}.geojson`)
+    .map((filename) => fs.createReadStream(path.join(__dirname, filename))
+      .pipe(JSONStream.parse('features.*')))
 
   // Some streets have no street name...
-  //   For example, in Manhttan: 337, 940, 1106, 1394, 2058, 2662, 2702
+      //   For example, in Manhttan: 337, 940, 1106, 1394, 2058, 2662, 2702
 
   H(streams)
-    .map(stream => H(stream))
+    .map((stream) => H(stream))
     .merge()
     .filter(streetName)
     .group(streetName)
-    .toArray(function(groupsArray) {
-      var groups = groupsArray[0];
+    .toArray((groupsArray) => {
+      var groups = groupsArray[0]
       H(R.values(groups))
         .map(convertFeatures)
         .compact()
-        .map(H.curry(writeObjects, writer))
+        .flatten()
+        .map(H.curry(tools.writer.writeObject))
         .nfcall([])
         .series()
-        .stopOnError(function(err) {
-          callback(err);
-        })
-        .done(function() {
-          callback();
-        });
-    });
+        .stopOnError(callback)
+        .done(callback)
+    })
 }
 
 // ==================================== API ====================================
 
 module.exports.steps = [
-  convert
-];
+  transform
+]
